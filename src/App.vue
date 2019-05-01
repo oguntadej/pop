6daa632be97cbccb39cf413fd4599d381ca71843
Joshua Oguntade <joshua@joshuaoguntade.com.ng>
2019-05-01 17:28:50 +0000

    Initial commit for my personal website using Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div class="container">
+     <div class="row my-5">
+       <h1 class="col-md-12 display-4 py-4 font-weight-bold">Hello, I'm Joshua,
+         <small>I build products for the web.</small></h1>
+       <div class="col-md-3 pt-5 text-center">
+         <img src="./assets/portrait.jpg" class="img-fluid portrait">
+      </div>
+       <div class="col-md-6 py-5 about">
+         <p>
+           With my background in engineering, I'm a systems thinker that
+           naturally gravitates towards strategy and product problems.
+           For now, however, I'm focusing on fine-tuning my craft
+           skills — crafting digital experiences that help people using various
+           tools and languages such as PHP, Javascript and Ruby.
+         </p>
+         <p>
+           These days, I am working on engineering,
+           product development and business strategy at
+           Pennysmart, a digital saving platform for African millennials.
+         </p>
+         <p>
+           I can generally be found listening to an unhealthy amount of music,
+           reading on various subjects, or trying to be <i class="font-weight-bold">JAY Z</i> cool.
+         </p>
+         <p><span v-if="book.records">I'm reading
+           <i class="font-weight-bold">{{currentlyReading}}</i> at the moment.</span>
+           Feel free to check out <a href="https://airtable.com/shrLvsksJPIW2H9ST" target="_blank">my library.</a> </p>
+       </div>
+       <div class="col-md-3 pt-5">
+         <iframe src="https://open.spotify.com/embed/user/jogunt/playlist/3MyW890R8ovmBFNGUiAd4s" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+       </div>
+     </div>
+   </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      book: [],
+    };
+  },
+  methods: {
+    fetchCurrentlyReading() {
+      const uri = 'https://api.airtable.com/v0/appE7rDWQTMztAodU/Books';
+      const token = process.env.VUE_APP_AIRTABLE_TOKEN;
+      axios.get(uri, { headers: { Authorization: `Bearer ${token}` }, params: { fields: ['Title'], filterByFormula: 'AND({Status} = "Reading")' } })
+        .then((response) => { this.book = response.data; }).catch(error => (console.log(token)));
+    },
+  },
+  computed: {
+    currentlyReading() {
+      let currentlyReading = 'nothing';
+      if (this.book.records.length > 0) {
+        currentlyReading = this.book.records[0].fields.Title;
+      }
+      return currentlyReading;
+    },
+  },
+  mounted() {
+    this.fetchCurrentlyReading();
   },
 };
 </script>
@@ -21,8 +75,16 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #dddddd;
 }
+  .background-dark{
+    background-color: rgb(10,10,10);
+  }
+  .portrait{
+    width: 300px;
+    height: auto;
+  }
+  .about{
+    text-align: justify;
+  }
 </style>
